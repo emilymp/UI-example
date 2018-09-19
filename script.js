bea2b15fe6950acf8d1586ec51f864da451250ab
emilymp <emilyperi2@gmail.com>
2018-09-19 13:28:52 -0700

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function toggleVis() {
+  var answer = document.getElementById('answer');
+  var button = document.getElementById('answerButton')
+  var currentDisplay = answer.style.display;
 
+  if (currentDisplay === 'block') {
+    answer.style.display = 'none';
+    button.innerText = 'Show Answer';
+  }
+
+  else {
+    answer.style.display = 'block';
+    button.innerText = 'Hide Answer';
+
+  }
+}
